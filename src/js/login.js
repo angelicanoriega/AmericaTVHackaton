207cf2990c787f5ad2726a5b145fa30c696573c8
Nataly Porras <natalypc27@gmail.com>
2018-10-30 12:36:56 -0500

    pantalla principal

---
+++ src/js/login.js
@@ -0,0 +1,36 @@
+const guardaDatos = (user) => {
+  let usuario = {
+    uid: user.uid,
+    nombre: user.displayName,
+    email: user.email,
+    foto: user.photoURL,
+  }
+  firebase.firestore().ref('Users/' + user.uid)
+  .set(usuario)
+}
+
+const close = () => {
+    firebase.auth().signOut()
+    .then(() => alert('Saliendo...'))
+    .catch((error) => console.log(error))
+  };
+const register1 = (email, password) => {
+  firebase.auth().createUserWithEmailAndPassword(email, password)
+    .ther(result => {
+      const user = {
+        uid: result.user.uid,
+        displayName: email_login.value,
+        email: result.user.email,
+        photoURL: 'http:subirimagen.me/uploads/20180725011911.png',
+        // type:
+      }
+      guardaDatos(user)
+  })
+}
+
+const login1 = (email, password) => {
+  firebase.auth().signInWithEmailAndPassword(email, password)
+    .then(result => {
+      alert('hola mundo')
+    })
+}
--- src/js/main.js
+++ src/js/main.js
@@ -1,7 +1,23 @@
-const loginAdmin=document.getElementById("login");
-const getEmail=document.getElementById("email_login");
-const getPassword=document.getElementById("password_login")
+const name = document.getElementById('name');
+const email = document.getElementById('email');
+const password = document.getElementById('password');
+const register = document.getElementById('register1111');
 
-loginAdmin.addEventListener('click', () => {
-    signInUser(getEmail.value, getPassword.value);
+// LOGIN
+const login = document.getElementById('login')
+const email_login = document.getElementById('email_login')
+const passwordLogin = document.getElementById('passwordLogin');
+
+
+window.onload = () => {
+  firebase.auth().onAuthStateChanged(user => {
+    if (user) {
+      window.location = "calendario.html"
+    }
   })
+}
+
+register.addEventListener('click', () => {
+  register1(email.value, password.value)
+})
+
