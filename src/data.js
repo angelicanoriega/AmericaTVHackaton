b83491bc74dbeaf1a1f24afc797ebf903acb342e
angiecortez <angiecorteztay1@hotmail.com>
2018-10-30 10:09:52 -0500

    add firebase

---
+++ src/data.js
@@ -0,0 +1,9 @@
+const config = {
+  apiKey: "AIzaSyB5pMprNuEYsFu1IG5HC0XR6B6SCp_Ax40",
+  authDomain: "americatv-9ab5d.firebaseapp.com",
+  databaseURL: "https://americatv-9ab5d.firebaseio.com",
+  projectId: "americatv-9ab5d",
+  storageBucket: "americatv-9ab5d.appspot.com",
+  messagingSenderId: "309230839115"
+};
+firebase.initializeApp(config);
